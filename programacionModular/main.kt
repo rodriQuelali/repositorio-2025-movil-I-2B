d492038cb915ed3893405d1e4e97843189133c53
rodriquelali <rodriquelali@gmail.com>
2025-02-25 21:57:55 -0400

    Programacion Modular.

---
+++ programacionModular/main.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    //programacion modular
+
+    //funciones 
+    //procedimientos
+
+
+}
+//procedimiento
+fun areaCuadrado(a:Int, mesaje:String ="hola"){
+    println("calcular area $mesaje: ${a*a}")
+}
+//funcion
+fun perimetroCuadrado(a:Int):Int = 4*a
