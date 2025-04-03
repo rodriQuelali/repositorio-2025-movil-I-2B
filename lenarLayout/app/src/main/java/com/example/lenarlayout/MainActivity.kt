dd16f341ac886eeff5d74fb33d537f14f56bb2e0
rodriquelali <rodriquelali@gmail.com>
2025-04-02 21:05:18 -0400

    Add carView.

--- lenarLayout/app/src/main/java/com/example/lenarlayout/MainActivity.kt
+++ lenarLayout/app/src/main/java/com/example/lenarlayout/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lenarlayout
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    //funcio de lalamdo a otra actividad.
+    fun llamarActivi(view: View){
+        val i = Intent(this, inicio::class.java)
+        startActivity(i)
     }
 }
--- lenarLayout/app/src/main/java/com/example/lenarlayout/inicio.kt
+++ lenarLayout/app/src/main/java/com/example/lenarlayout/inicio.kt
@@ -1,6 +1,8 @@
 package com.example.lenarlayout
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class inicio : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun llamarActiviMain(view: View){
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
     }
 }
