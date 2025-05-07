da8cddba714ac44583b77da4d73ddc41f40b8c0a
rodriquelali <rodriquelali@gmail.com>
2025-05-06 21:47:43 -0400

    Sobrecarga de metodos suma.

--- calculadoraPOO/app/src/main/java/com/example/calculadorapoo/data/Calculadora.kt
+++ calculadoraPOO/app/src/main/java/com/example/calculadorapoo/data/Calculadora.kt
@@ -1,19 +1,22 @@
 package com.example.calculadorapoo.data
 
-class Calculadora constructor(num1: Int, num2: Int){
+class Calculadora {
 
     /*constructor(num1: Int, num2: Int){
         this.num1 = num1
         this.num2 = num2
     }*/
 
-    private val num1: Int = num1
-    private val num2: Int = num2
-    private var resultado: Int = 0
 
-    fun sumar():Int{
-        this.resultado = this.num1 + this.num2
-        return this.resultado
+    fun sumar(a: Int, b: Int):Int{
+        return a + b
+    }
+
+    //sobrecarga de metodos
+
+
+    fun suma(a :Float, b:Float): Float{
+        return a + b
     }
 
     /*
