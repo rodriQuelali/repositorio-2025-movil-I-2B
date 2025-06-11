91f77b4d613be51ea1d36c0d6c78a4b3cf1cd25d
rodriquelali <rodriquelali@gmail.com>
2025-06-10 22:01:56 -0400

    Operar radio suma and resta

--- calculadoraPOO/app/src/main/java/com/example/calculadorapoo/RadioCkeck.kt
+++ calculadoraPOO/app/src/main/java/com/example/calculadorapoo/RadioCkeck.kt
@@ -1,10 +1,16 @@
 package com.example.calculadorapoo
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculadorapoo.data.Calculadora
 
 class RadioCkeck : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,27 @@ class RadioCkeck : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val rbSuma = findViewById<RadioButton>(R.id.rbSuma)
+        val rbResta = findViewById<RadioButton>(R.id.rbResta)
+
+        val txt1 = findViewById<EditText>(R.id.txt1)
+        val txt2 = findViewById<EditText>(R.id.edtxt2)
+
+        val tvR = findViewById<TextView>(R.id.tvResul)
+
+        val btnOpe = findViewById<Button>(R.id.btnOpeRadio)
+
+
+        btnOpe.setOnClickListener {
+            if(rbSuma.isChecked == true){
+                tvR.text = Calculadora().suma(txt2.text.toString().toDouble(), txt1.text.toString().toDouble()).toString()
+            }
+
+            if(rbResta.isChecked == true){
+                tvR.text = Calculadora().resta(txt2.text.toString().toDouble(), txt1.text.toString().toDouble())
+            }
         }
     }
 }
