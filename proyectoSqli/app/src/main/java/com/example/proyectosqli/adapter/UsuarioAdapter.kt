59a05c3fdd361c77dc1e685eb79e030e1b1c105f
rodriquelali <rodriquelali@gmail.com>
2025-10-28 21:00:11 -0400

    Actualiazacion nyevo.

---
+++ proyectoSqli/app/src/main/java/com/example/proyectosqli/adapter/UsuarioAdapter.kt
@@ -0,0 +1,8 @@
+package com.example.proyectosqli.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectosqli.model.Usuario
+
+class UsuarioAdapter(private val listusuario: List<Usuario>) : RecyclerView.Adapter<>{
+
+}
