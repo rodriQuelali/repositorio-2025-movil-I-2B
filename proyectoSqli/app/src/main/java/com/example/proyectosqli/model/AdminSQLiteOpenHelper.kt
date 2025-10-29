6acfb5cffb13afc6b33ddb1b33aeeb320ece048f
rodriquelali <rodriquelali@gmail.com>
2025-10-29 19:54:32 -0400

    Add SQLite.

---
+++ proyectoSqli/app/src/main/java/com/example/proyectosqli/model/AdminSQLiteOpenHelper.kt
@@ -0,0 +1,18 @@
+package com.example.proyectosqli.model
+
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class AdminSQLiteOpenHelper : SQLiteOpenHelper() {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(
+        p0: SQLiteDatabase?,
+        p1: Int,
+        p2: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+}
